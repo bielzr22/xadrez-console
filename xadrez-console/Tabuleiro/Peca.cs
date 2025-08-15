557b2b42466f59c2289e1cde092149b527133a7a
Gabriel Coelho <gabrielcoelho427@gmail.com>
2025-08-15 00:11:43 -0300

    Comecando a implementar o tabuleiro

---
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtdadeMovimentos { get; protected set; }
+        public Tabuleiro Tabuleiro { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro )
+        {
+            Posicao = posicao;
+            Cor = cor;
+            QtdadeMovimentos = 0;
+            Tabuleiro = tabuleiro;
+        }
+    }
+}
